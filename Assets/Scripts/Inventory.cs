364c2b1a8091bc8eb0ce1fd113adb79f4ebf5a43
Daniel <dsotnikov20@gmail.com>
2024-07-24 16:00:15 +0300

    Array cell creation

--- Assets/Scripts/Inventory.cs
+++ Assets/Scripts/Inventory.cs
@@ -5,16 +5,16 @@ public class Inventory : MonoBehaviour
     public GameObject[] _prefab;
     public GameObject _panel;
 
-    private int _inventorySlotsLength = 10;
+    private GameObject[] _inventorySlotsLength = new GameObject[20];
 
     private void Start() 
     {
         AddInventorySlots(_inventorySlotsLength);
     }
 
-    public void AddInventorySlots(int numberSlots) 
+    public void AddInventorySlots(GameObject[] numberSlots) 
     {
-        for(int i = 0; i < numberSlots; i++) 
+        for(int i = 0; i < numberSlots.Length; i++) 
         {   
             Instantiate(_prefab[i], _panel.transform);
         }
